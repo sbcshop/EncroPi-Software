055019c51c4c16aa636728bf12451e18378ba8cf
sbcshop2 <120171682+sbcshop2@users.noreply.github.com>
2022-12-22 18:03:59 +0530

    Add files via upload

---
+++ gpio_test.py
@@ -0,0 +1,15 @@
+from machine import Pin
+import utime,time
+
+status = machine.Pin(25, machine.Pin.OUT) #gp25 is used for status led
+gp4 = machine.Pin(4, machine.Pin.OUT) 
+gp5 = machine.Pin(5, machine.Pin.OUT)
+gp14 = machine.Pin(14, machine.Pin.OUT)
+gp15 = machine.Pin(15, machine.Pin.OUT)
+
+status.value(1)
+gp4.value(1)
+gp5.value(1)
+gp14.value(1)
+gp15.value(1)
+
